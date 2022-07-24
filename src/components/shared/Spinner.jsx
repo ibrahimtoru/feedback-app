192fe7f9c97f01bbf34c459cb9a7cc88b0563458
ibrahimtoru <64074146+ibrahimtoru@users.noreply.github.com>
2022-07-24 20:53:43 +0500

    finsihed project

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from "../assets/loader.gif";
+
+export default function Spinner(props) {
+  return (
+    <img
+      src={spinner}
+      alt="Loading..."
+      style={{ width: "100px", margin: "auto", display: "block" }}
+    />
+  );
+}
